afa43fb633885c18050fd683bc26af101641ca6f
yui <yy523697597@users.noreply.github.com>
2017-11-05 10:04:14 +0800

    V1.2 引入axios用于数据交互

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import fastclick from 'fastclick';
+import axios from 'axios';
 
 import './common/scss/index.css';
 
 fastclick.attach(document.body);
 
 Vue.config.productionTip = false;
-
+Vue.prototype.$http = axios;
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
