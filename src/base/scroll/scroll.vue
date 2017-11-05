fe98a630439b9adde97fd838f96732522d798acb
yui <yy523697597@users.noreply.github.com>
2017-11-05 22:11:55 +0800

    V1.5 基本完成推荐页面的开发

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,63 @@
+<template>
+  <div ref="wrapper">
+      <slot></slot>
+  </div>
+</template>
+<script>
+import BScroll from 'better-scroll';
+export default {
+  props: {
+    probeType: {
+      type: Number,
+      default: 1
+    },
+    click: {
+      type: Boolean,
+      default: true
+    },
+    data: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this._initScroll();
+    });
+  },
+  methods: {
+    _initScroll() {
+      if (!this.$refs.wrapper) {
+        return;
+      }
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        probeType: this.probeType,
+        click: this.click
+      });
+    },
+    enable() {
+      this.scroll && this.scroll.enable();
+    },
+    disable() {
+      this.scroll && this.scroll.disable();
+    },
+    refresh() {
+      this.scroll && this.scroll.refresh();
+    }
+  },
+  // 追踪传入的data的变化,data变化后就刷新scroll
+  watch: {
+    data() {
+      this.$nextTick(() => {
+        this.refresh();
+      });
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
+
--- src/pages/recommend/recommend.vue
+++ src/pages/recommend/recommend.vue
@@ -1,42 +1,60 @@
 <template>
   <div class="recommend">
-      <div class="recommend-content">
-        <div class="slider-wrapper"></div>
-        <div class="recommend-list">
-          <!-- 需要有banners的数据之后再去渲染DOM,不然的话slider的宽度不对 -->
-          <slider v-if="banners.length">
-            <div v-for="(item,index) of banners" :key="index">
-              <div @click="_bannerClick(item.typeTitle,item.url)">
-                <img :src="item.pic">
-              </div>
-              <span class="type">{{item.typeTitle}}</span>
+      <scroll ref="scroll" :data="playLists" class="recommend-content">
+        <div>
+            <div class="slider-wrapper">
+              <!-- 需要有banners的数据之后再去渲染DOM,不然的话slider的宽度不对 -->
+              <slider v-if="banners.length">
+                <div v-for="(item,index) of banners" :key="index">
+                  <div @click="_bannerClick(item.typeTitle,item.url)">
+                    <img @load="loadImg" :src="item.pic">
+                  </div>
+                  <span class="type">{{item.typeTitle}}</span>
+                </div>
+              </slider>
             </div>
-          </slider>
-        <div class="list-title">热门歌单推荐</div>
-        <ul></ul>
-      </div>
-      </div>
-      
+            <div class="recommend-list">
+              <h1 class="list-title">热门歌单推荐</h1>
+              <ul>
+                <li class="item" v-for="(item,index) of playLists" :key="index">
+                  <div class="icon">
+                    <img :src="item.coverImgUrl" >
+                  </div>
+                  <div class="text">
+                    <h2 class="name">{{item.creator.nickname}}</h2>
+                    <p class="desc">{{item.name}}</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+        </div>
+      </scroll>
   </div>
 </template>
 <script>
 import { ERR_OK } from 'api/config';
 import Slider from 'base/slider/slider';
+import Scroll from 'base/scroll/scroll';
+
 export default {
   data() {
     return {
-      banners: []
+      banners: [],
+      playLists: [],
+      loadedImg: false
     };
   },
   components: {
-    Slider
+    Slider,
+    Scroll
   },
   created() {
-    this._getRecommend();
+    this._getBanners();
+    this._getPlaylists();
   },
   methods: {
-    // 获取推荐数据
-    _getRecommend() {
+    // 获取banner数据
+    _getBanners() {
       // 获取banner数据
       const url = this.HOST + 'banner';
       this.$http.get(url).then(res => {
@@ -45,10 +63,26 @@ export default {
         }
       });
     },
+    // 获取推荐歌单
+    _getPlaylists() {
+      const url = this.HOST + 'top/playlist';
+      this.$http.get(url).then(res => {
+        if (res.status === ERR_OK) {
+          this.playLists = res.data.playlists;
+        }
+      });
+    },
     // banner点击事件
     _bannerClick(type, url) {
       if (url !== '') {
         window.location = url;
+      }
+    },
+    // banner图片载入后,刷新scroll
+    loadImg () {
+      if (!this.loadedImg) {
+        this.loadedImg = true;
+        this.$refs.scroll.refresh();
       }
     }
   }
@@ -62,17 +96,16 @@ export default {
   width: 100%;
   top: 1.76rem;
   bottom: 0;
-
   .recommend-content {
-    height: 100%;
-    overflow: hidden;
+  height: 100%;
+  overflow: hidden;
     .slider-wrapper {
       position: relative;
       width: 100%;
       overflow: hidden;
-    }
+  }
 
-    .recommend-list {
+  .recommend-list {
       .list-title {
         height: 1.3rem;
         line-height: 1.3rem;
@@ -89,8 +122,11 @@ export default {
           flex: 0 0 1.2rem;
           width: 1.2rem;
           padding-right: 0.4rem;
+          img {
+            width: 1.2rem;
+            height: 1.2rem;
+          }
         }
-
         .text {
           display: flex;
           flex-direction: column;
@@ -108,14 +144,14 @@ export default {
           }
         }
       }
-    }
+  }
 
-    .loading-container {
+  .loading-container {
       position: absolute;
       width: 100%;
       top: 50%;
       transform: translateY(-50%);
-    }
   }
+}
 }
 </style>
