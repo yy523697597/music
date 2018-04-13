f98d1da70c8bb533b8ea545606c14f54995ec4c9
yuyi <523697597@qq.com>
2018-04-13 16:55:05 +0800

    删除m-header

--- src/App.vue
+++ src/App.vue
@@ -9,21 +9,19 @@
 </template>
 
 <script>
-import MHeader from 'components/m-header/m-header';
 import Tab from 'components/tab/tab';
 import Player from 'pages/player/player';
-// import { getQueryString } from '~common/js/util';
 
 export default {
   name: 'app',
   components: {
-    MHeader,
     Tab,
     Player
   },
   created() {
     // 通过判断参数来确定需不需要显示vconsole
     const userQuery = '_debug';
+
     const showConsole = this.$route.query[userQuery];
     if (showConsole) {
       require('./assets/util/vconsole.js');
